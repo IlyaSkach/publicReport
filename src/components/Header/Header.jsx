d6f534dc6c67e84e35232a8465afd862bc9d91b8
Илья С <skachilya@gmail.com>
2024-06-17 14:34:57 +0300

    Add frontend directory

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
+// add user
+
 const Header = () => {
   return (
     <>
       <header className="header">
         <div className="header__title">
-          Пользователь: <span className="header__user">Admin</span>
+          Пользователь: <span className="header__user">Admin</span>  
         </div>
       </header>
     </>
